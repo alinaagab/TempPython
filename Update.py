95978dfa15b80ac01da85101bd76601fe8abfe97
alinaagab <50396865+alinaagab@users.noreply.github.com>
2019-11-15 14:01:55 +0100

    Create Update.py

---
+++ Update.py
@@ -0,0 +1,29 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+
+
+blue = (0, 0, 255)
+red = (255,0,0)
+
+while True:
+
+  
+  t = sense.get_temperature()
+  h = sense.get_humidity()
+
+  # Round the values to one decimal place
+  t = round(t, 1)
+  h = round(h, 1)
+  
+  
+  # Create the message
+  # str() converts the value to a string so it can be concatenated
+  message = "Temperature: " + str(t)  + " Humidity: " + str(h)
+  
+  if  t < 25.7:
+    bg = blue
+  else:
+    bg = red
+  
+
+  sense.show_message(message, scroll_speed=0.05, back_colour=bg)
